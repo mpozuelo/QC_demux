b418c0aac8d1ae35cf1ad180d1ec2b2fa7c9fd28
mpozuelo <mpozud00@gmail.com>
2021-02-13 09:09:37 +0100

    correct xrange to range

--- bin/fastq.py
+++ bin/fastq.py
@@ -40,7 +40,7 @@ class Reader:
 
        lines = []
        #read 4 (lines, name, sequence, strand, quality)
-       for i in xrange(0,4):
+       for i in range(0,4):
            line = self.__file.readline().rstrip()
            if len(line) == 0:
                self.__eof = True
