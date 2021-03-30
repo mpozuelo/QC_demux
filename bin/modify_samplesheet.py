51df227f2e28d3bc42c6071b5a15f3fb237411c7
mpozuelo <mpozud00@gmail.com>
2021-03-30 18:04:49 +0200

    correct files path

--- bin/modify_samplesheet.py
+++ bin/modify_samplesheet.py
@@ -37,8 +37,8 @@ def add_bed_file(FileIn,FileOut):
     # Compute percentages
     cov['star_gtf'] = cov['genome'].map(star_gtf)
     cov['star_index'] = cov['genome'].map(star_index)
-    cov['fastq1'] = "/datos/ngs/dato-activo/data/04_pfastq/" + cov['platform'] + '/' + cov['machine'] + '/' + cov['run'] + '/' + cov['lane'] + '/' + cov['user'] + '/demux_fastq/' + cov['sampleID'] + '_' + cov['run'] + '_' + cov['lane'] + '_R1.fq.gz'
-    cov['fastq2'] = "/datos/ngs/dato-activo/data/04_pfastq/" + cov['platform'] + '/' + cov['machine'] + '/' + cov['run'] + '/' + cov['lane'] + '/' + cov['user'] + '/demux_fastq/' + cov['sampleID'] + '_' + cov['run'] + '_' + cov['lane'] + '_R2.fq.gz'
+    cov['fastq1'] = "/datos/ngs/dato-activo/data/04_pfastq/" + cov['platform'] + '/' + cov['machine'] + '/' + cov['run'] + '/' + cov['lane'] + '/' + cov['user'] + '/demux_fastq/' + cov['sampleID'] + '_S' + cov['ID'] + '_' + '_R1_001.fastq.gz'
+    cov['fastq2'] = "/datos/ngs/dato-activo/data/04_pfastq/" + cov['platform'] + '/' + cov['machine'] + '/' + cov['run'] + '/' + cov['lane'] + '/' + cov['user'] + '/demux_fastq/' + cov['sampleID'] + '_S' + cov['ID'] + '_' + '_R2_001.fastq.gz'
 
 
     cov.to_csv(fo, index = False)
